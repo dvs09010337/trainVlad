3f6e413f43f809b556d8ad392ff7f2675269e5fe
Bob <bob@gmail.com>
2022-04-21 20:37:49 +0300

    Created method Resize

--- MyProject/ConsoleApp1/Program.cs
+++ MyProject/ConsoleApp1/Program.cs
@@ -4,10 +4,48 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static void CreateArray(ref int [] arr, int size)
+        {
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = random.Next(1, 10);
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+        static void Resize(ref int [] arr, int newSize)
+        {
+            var newArr = new int[newSize];
+            if (newSize > arr.Length)            
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < newSize; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+            }
+            for (int i = 0; i < newSize ; i++)
+            {
+                Console.Write(newArr[i] + " ");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("влад");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int [] array = new int[n];
+            CreateArray(ref array, n);
+            Console.WriteLine();
+            int newSize = Convert.ToInt32(Console.ReadLine());
+            Resize(ref array, newSize);
         }
     }
 }
